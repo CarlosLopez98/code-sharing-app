a6adad22eb1e37cd398f29dc36669be8ec87b314
carloslopez98 <carlos980718@gmail.com>
2025-03-13 17:19:30 -0500

    feat: Initial setting for the spring boot project

---
+++ api/src/main/java/com/carlos/notecode/NotecodeApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.notecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotecodeApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/carlos/notecode/NotecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.notecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
